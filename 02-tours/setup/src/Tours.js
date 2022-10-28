5ff652518c5392f41c217013f6fdeec3ea76fce2
Nika Kobaidze <nika.kobaidze@gmail.com>
2022-10-28 20:04:04 +0000

    [ADD] tour list

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours }) => {
+    return (
+        <>
+            <div className="title">
+                <h2>Our torus</h2>
+                <div className="underline"></div>
+            </div>
+            <div>
+                {tours.map((tour) => {
+                    console.log(tour);
+                    return <Tour key={tour.id} {...tour}></Tour>;
+                })}
+            </div>
+        </>
+    );
 };
 
 export default Tours;
