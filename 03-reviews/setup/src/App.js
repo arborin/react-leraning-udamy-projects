c8f70b96584aa21a980f97008dd6c0f7df2b4148
Nika Kobaidze <nika.kobaidze@gmail.com>
2022-10-30 19:23:23 +0000

    [ADD] reviewers project

--- 03-reviews/setup/src/App.js
+++ 03-reviews/setup/src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
-import Review from './Review';
+import React from "react";
+import Review from "./Review";
+import { FaCanadianMapleLeaf } from "react-icons/fa";
+
 function App() {
-  return <h2>reviews project setup</h2>;
+    return (
+        <main>
+            <section className="container">
+                <div className="title">
+                    <h2>Our reviewers</h2>
+                    <div className="underline"></div>
+                </div>
+                <Review />
+            </section>
+        </main>
+    );
 }
 
 export default App;
