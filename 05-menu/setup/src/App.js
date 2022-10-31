58ab1334b3209f1f668bcb925f85bc8e4c6a3faf
nkobaidze <nika.kobaidze@gmail.com>
2022-10-31 16:53:18 +0400

    [ADD] category filter

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -7,13 +7,22 @@ function App() {
     const [menuItems, setMenuItems] = useState(items);
     const [categories, setCategories] = useState([]);
 
+    const filterItems = (category) => {
+        console.log(category);
+        const newItems = items.filter((item) => {
+            return item.category === category;
+        });
+
+        setMenuItems(newItems);
+    };
+
     return (
         <main>
             <div className="title">
                 <h2>our menu</h2>
                 <div className="underline"></div>
 
-                <Categories />
+                <Categories filterItems={filterItems} />
                 <Menu items={menuItems} />
             </div>
         </main>
