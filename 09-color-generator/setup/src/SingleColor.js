b8c4399422d222762f9393cfcc6cdb1d0789d8c7
nkobaidze <nika.kobaidze@gmail.com>
2022-11-16 17:48:18 +0400

    [ADD] copy clipboard

--- 09-color-generator/setup/src/SingleColor.js
+++ 09-color-generator/setup/src/SingleColor.js
@@ -6,12 +6,28 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
     const bcg = rgb.join(",");
     const hex = rgbToHex(...rgb);
 
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setAlert(false);
+        }, 300);
+
+        return () => clearTimeout(timeout);
+    }, [alert]);
+
     console.log(bcg);
     const style = { backgroundColor: `rgb(${bcg})` };
     return (
-        <article className={`color`} style={style}>
+        <article
+            className={`color`}
+            style={style}
+            onClick={() => {
+                setAlert(true);
+                navigator.clipboard.writeText(hex);
+            }}
+        >
             <p className="percent-value">{weight} %</p>
             <p>{hex}</p>
+            {alert && <p className="alert">Copy to clipboard</p>}
         </article>
     );
 };
