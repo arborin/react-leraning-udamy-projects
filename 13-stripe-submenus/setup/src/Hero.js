996582ac29693a67ea75c5dc6676ae3bd468f829
nkobaidze <nika.kobaidze@gmail.com>
2022-12-14 17:17:39 +0400

    [ADD] navbar hero

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from "react";
+import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+    const { closeSubmenu } = useGlobalContext();
 
-export default Hero
+    return (
+        <section className="hero">
+            <div className="hero-center">
+                <article className="hero-info">
+                    <h1>test test</h1>
+                    <p>dsad asdas das dasd asd asd</p>
+                    <button className="btn">Start Now</button>
+                </article>
+                <article className="hero-images">
+                    <img src={phoneImg} className="phone-img" alt="phone" />
+                </article>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
